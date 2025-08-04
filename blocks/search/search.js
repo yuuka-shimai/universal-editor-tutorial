1cd51af0a2f3d3da7fc3deb98becba3fdb326f9c
yuuka-shimai <yshimai@adobe.com>
2025-08-04 23:24:10 +0900

    fix: search

--- blocks/search/search.js
+++ blocks/search/search.js
@@ -1,7 +1,10 @@
+import { readBlockConfig } from '../../scripts/scripts.js';
+
 export default function decorate(block) {
-  console.log(JSON.stringify(block));
-  // データ属性またはedsの渡し方によって取得方法が異なる場合があります
-  const placeholder = block.dataset.placeholder || 'Search....';
+  const config = readBlockConfig(block);
+  console.log('[Block Config]', config); // { placeholder: '検索してください' }
+
+  const placeholder = config.placeholder || 'Search.....';
 
   const wrapper = document.createElement('div');
   wrapper.className = 'search-block-wrapper';
@@ -11,13 +14,8 @@ export default function decorate(block) {
   input.className = 'search-input';
   input.placeholder = placeholder;
 
-  // 必要に応じて検索ボタン等も追加できます
-  // const button = document.createElement('button');
-  // button.innerText = '検索';
-
   wrapper.appendChild(input);
-  // wrapper.appendChild(button);
-
   block.innerHTML = '';
   block.appendChild(wrapper);
 }
+
